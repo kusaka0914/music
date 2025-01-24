c1d4481932d837ef2661a43344a816f911b339d2
Takumi Kusaka <your.email@example.com>
2025-01-24 20:36:10 +0900

    homeレスポンシブ

--- music/settings.py
+++ music/settings.py
@@ -13,4 +13,20 @@ INSTALLED_APPS = [
 NGROK_URL = ""
 NGROK_ENABLED = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io'] 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io']
+
+# Cloudflareの設定
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# 静的ファイルの設定
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# WhiteNoiseの追加
+MIDDLEWARE = [
+    # ... 既存のミドルウェア ...
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+] 
